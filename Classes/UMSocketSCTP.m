5c908b792faea75af1d8eec62a373eab615db30f
Andreas Fink <andreas@fink.org>
2018-05-22 09:55:28 +0200

    double

--- Classes/UMSocketSCTP.m
+++ Classes/UMSocketSCTP.m
@@ -775,10 +775,6 @@ int sctp_recvv(int s, const struct iovec *iov, int iovlen,
     struct sctp_sndrcvinfo sinfo;
     memset(&sinfo,0x00,sizeof(struct sctp_sndrcvinfo));
 
-
-    struct sctp_sndrcvinfo sinfo;
-    memset(&sinfo,0x00,sizeof(struct sctp_sndrcvinfo));
-
     sinfo.sinfo_stream = streamId;
     sinfo.sinfo_flags = 0;
     sinfo.sinfo_ppid = htonl(protocolId);
