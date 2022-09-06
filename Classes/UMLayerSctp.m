962b7ee09a4f955c551a1715df402bce5b1ec91a
Andreas Fink <andreas@fink.org>
2022-09-06 11:52:16 +0200

    wip

--- Classes/UMLayerSctp.m
+++ Classes/UMLayerSctp.m
@@ -913,12 +913,12 @@
             {
                 UMSocketError err = UMSocketError_no_error;
                 _directSocket = [_listener peelOffAssoc:rx.assocId error:&err];
-                [_layerHistory addLogEntry:[NSString stringWithFormat:@"processReceivedData: peeling off assoc %lu into socket %p/%d err=%d",(unsigned long)_assocId.unsignedLongValue,_directSocket,_directSocket.sock,err]];
+                [_layerHistory addLogEntry:[NSString stringWithFormat:@"processReceivedData: peeling off assoc %lu into socket %p/%d err=%d/%@",(unsigned long)_assocId.unsignedLongValue,_directSocket,_directSocket.sock,err,[UMSocket getSocketErrorString:err]]];
                 if((err != UMSocketError_no_error) && (err !=UMSocketError_in_progress))
                 {
                     [_directSocket close];
                     [_listener unregisterAssoc:_assocId forLayer:self];
-                    _assocId=NULL;
+                    _assocId = NULL;
                     _directSocket = NULL;
                     NSString *s = [NSString stringWithFormat:@"processReceivedData peeloff failed"];
                     [self logMinorError:s];
@@ -1070,7 +1070,7 @@
         {
             UMSocketError err = UMSocketError_no_error;
             _directSocket = [_listener peelOffAssoc:_assocId error:&err];
-            NSString *s=[NSString stringWithFormat:@"processReceivedData: peeling off assoc %u into socket %p/%d err=%d",ass,_directSocket,_directSocket.sock,err];
+            NSString *s=[NSString stringWithFormat:@"processReceivedData: peeling off assoc %u into socket %p/%d err=%d/%@",ass,_directSocket,_directSocket.sock,err,[UMSocket getSocketErrorString:err]];
             NSLog(@"%@",s);
             [_layerHistory addLogEntry:s];
             if((err != UMSocketError_no_error) && (err !=UMSocketError_in_progress))
